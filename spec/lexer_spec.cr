dd4e4061ec88a037cf175c178a5e3f35f8ebdd99
Zac Nowicki <zachnowicki@gmail.com>
2017-10-11 20:19:35 +0000

    spec lexing for LargeTuple, List, Map

--- spec/lexer_spec.cr
+++ spec/lexer_spec.cr
@@ -28,22 +28,28 @@ private def it_lexes_type_with_value(expected_type, value, bytes, file = __FILE_
       token.binary_value.should eq(value)
     when Type::Float
       token.float_value.should eq(value)
-    when Type::SmallTuple
+    when Type::SmallTuple, Type::LargeTuple, Type::List
       token.size.should eq(value)
     end
   end
 end
 
 describe Lexer do
+  # Basic terms
   it_lexes_type(Type::EOF, UInt8[])
   it_lexes_type(Type::Magic, UInt8[131])
   it_lexes_type(Type::Nil, UInt8[106])
 
+  # Single value terms
   it_lexes_type_with_value(Type::SmallInt, UInt8::MAX, UInt8[97, 255])
   it_lexes_type_with_value(Type::Integer, Int32::MAX, UInt8[98, 127, 255, 255, 255])
   it_lexes_type_with_value(Type::Atom, "abc", UInt8[100, 0, 3, 97, 98, 99])
   it_lexes_type_with_value(Type::Bin, Bytes[1, 2, 3], UInt8[109, 0, 0, 0, 3, 1, 2, 3])
   it_lexes_type_with_value(Type::Float, 1.234, UInt8[99, 49, 46, 50, 51, 52, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 101, 43, 48, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
+  # Complex terms
   it_lexes_type_with_value(Type::SmallTuple, 3, UInt8[104, 3])
+  it_lexes_type_with_value(Type::LargeTuple, 3, UInt8[105, 0, 3])
+  it_lexes_type_with_value(Type::List, 3, UInt8[108, 0, 0, 0, 3])
+  it_lexes_type_with_value(Type::Map, 3, UInt8[116, 0, 0, 0, 3])
 end
