728c6ea7768f23e1eba8a8f2873d64ee7c2bb8b7
Zac Nowicki <zachnowicki@gmail.com>
2017-10-17 07:33:05 -0400

    make a general exception for unpacking

--- src/bert/exceptions.cr
+++ src/bert/exceptions.cr
@@ -4,12 +4,20 @@ module BERT
   end
 
   # An error that occured within the `Lexer`
-  class LexerException < Error
+  class UnpackingException < Error
     # The byte number where the invalid token was read
     getter byte_number : Int32
 
     def initialize(message, @byte_number = 0)
       super "#{message} at #{@byte_number}"
     end
+  end
+
+  # An exception within the lexer
+  class LexerException < UnpackingException
+  end
+
+  # An exception within the parser
+  class ParserException< UnpackingException
   end
 end
